7f77d46020439d771795a9d532eb9fd549e97c5f
Ariane <xu.ariane@hotmail.fr>
2020-03-02 16:31:00 +0000

    add a cat and borders

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+
+get '/' do
+  'Hello world!'
+end
+
+get '/secret' do
+  "SSH!!!"
+end
+
+get '/monday' do
+  "Rise and Shine!"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed blue'>
+  <img src='http://bit.ly/1eze8aE'>
+ </div>"
+end
