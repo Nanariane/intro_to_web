f8d6dab9a5049312c28c64758d7727da11a6911f
Ariane <xu.ariane@hotmail.fr>
2020-03-03 11:37:56 +0000

    Keeping the views clean

--- app.rb
+++ app.rb
@@ -1,22 +1,23 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-
-get '/' do
-  'Hello world!'
-end
-
-get '/secret' do
-  "SSH!!!"
-end
-
-get '/monday' do
-  "Rise and Shine!"
-end
+# get '/' do
+#   'Hello world!'
+# end
+#
+# get '/secret' do
+#   "SSH!!!"
+# end
+#
+# get '/monday' do
+#   "Rise and Shine!"
+# end
 
 get '/cat' do
+# We have moved this bit to the index.erb file (Sinatra: Keeping the views clean)
  #  "<div style='border: 3px dashed blue'>
  #  <img src='http://bit.ly/1eze8aE'>
  # </div>"
- erb(:index)
+  @name = ["Lion", "Sphinx", "Phoenix"].sample
+ erb :index
 end
